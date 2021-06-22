dcb880afb5cde78b58b9fca5adaad43f9e837e2b
Everton <everton.aaraujo@outlook.com>
2021-06-22 20:05:47 -0300

    Auth refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router';
 
-import { AuthContext } from '../App';
 import { Button } from '../components/Button';
+
+import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -12,7 +12,7 @@ import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if(!user) {
