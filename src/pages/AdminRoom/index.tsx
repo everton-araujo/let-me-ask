4608288ef55e174b2846cc39c02164e6a3ba5fd6
Everton <everton.aaraujo@outlook.com>
2021-06-28 20:20:08 -0300

    Like functionality created

---
+++ src/pages/AdminRoom/index.tsx
@@ -0,0 +1,60 @@
+import { useParams } from 'react-router';
+
+import { Button } from '../../components/Button';
+import { Question } from '../../components/Question';
+import { RoomCode } from '../../components/RoomCode';
+import { useRoom } from '../../hooks/useRoom';
+
+import logoImg from '../../assets/images/logo.svg';
+
+import '../../styles/room.scss';
+
+type RoomParams = {
+  id: string;
+}
+
+export function AdminRoom() {
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const {title, questions} = useRoom(roomId);
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          { 
+            questions.length > 0 && (
+              questions.length === 1 
+                ? <span>{questions.length} pergunta</span> 
+                : <span>{questions.length} perguntas</span> 
+            )
+          }
+        </div>
+
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question 
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
